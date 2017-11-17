9849eb6c4e34b9a69925b2e177190c33b2aee7e9
David Sanville <redbear@David-Sanvilles-MacBook-Pro.local>
2017-11-17 04:41:44 -0800

    added tree file

---
+++ tree.js
@@ -0,0 +1,17 @@
+
+function Node(data) {
+    this.data = data;
+    this.next = null;
+}
+
+class tree {
+  constructor(board, data) {
+    this.node = data;
+    //up to as many children as possible moves
+    //from that board state
+    this.child[] = null;
+  }
+}
+
+
+foo = new tree(board)
